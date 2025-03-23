3db9bd336c67eb2391af1e74ce733f47b676981f
bot001yt <97733546+bot001yt@users.noreply.github.com>
2025-03-23 16:46:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,5 +18,14 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
+    
+@bot.command()
+async def addventa(ctx, fecha, categoria, producto, proveedor, compra, venta, canal, estado):
+    try:
+        add_sale_row(fecha, categoria, producto, proveedor, compra, venta, canal, estado)
+        await ctx.send("✅ Venta registrada correctamente.")
+    except Exception as e:
+        await ctx.send(f"❌ Error al registrar la venta: {e}")
+
 
 bot.run(TOKEN)
